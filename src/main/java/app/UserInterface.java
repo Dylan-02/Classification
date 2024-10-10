584dac87a8408b73d1cd7f27cae8ce61b4b98c55
Camille Okubo <camille.okubo.etu@univ-lille.fr>
2024-10-10 13:26:25 +0200

    Avancée interface

--- src/main/java/app/UserInterface.java
+++ src/main/java/app/UserInterface.java
@@ -1,14 +1,18 @@
 package app;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class UserInterface extends Stage {
@@ -16,20 +20,38 @@ public class UserInterface extends Stage {
     //Que du visuel pour l'instant, commentaire à retirer
     public UserInterface(){
 
+        this.setMinWidth(610);
+
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
 
-        TextField filePathTf = new TextField("Sélectionner fichier");
+        Button boutonFichier = new Button("Choisir fichier");
+        Label cheminFichier = new Label("aucun fichier selectionné");
+        HBox boxFichier = new HBox(boutonFichier, cheminFichier);
+
+        FileChooser filePathTf = new FileChooser(); //TODO relier au bouton
         ScatterChart<Number, Number> chart = new ScatterChart<>(xAxis, yAxis);
+        chart.prefHeightProperty().bind(this.heightProperty().subtract(100));
         Button boutonClassifier = new Button("Classifier");
-        VBox chartBox = new VBox(filePathTf, chart, boutonClassifier);
+
+        VBox chartBox = new VBox(boxFichier, chart, boutonClassifier);
+        chartBox.prefWidthProperty().bind(this.widthProperty().subtract(100));
 
         Text axeDesAbscisses = new Text("Axe des abscisses");
         ComboBox<String> menuDeroulantAbscisses = new ComboBox<>(); //TODO à l'implémentaiton des données, CHANGER LE TYPE GéNéRIQUE DES COMBOBOX
         Text axeDesOrdonnees = new Text("Axe des ordonnées");
         ComboBox<String> menuDeroulantOrdonnees = new ComboBox<>();
+
+
         VBox sideBar = new VBox(axeDesAbscisses, menuDeroulantAbscisses, axeDesOrdonnees, menuDeroulantOrdonnees);
+        sideBar.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+        sideBar.setPadding(new Insets(5));
+        sideBar.setAlignment(Pos.BASELINE_RIGHT);
+        sideBar.setMinWidth(400);
+
+
         HBox mainBox = new HBox(chartBox, sideBar);
+
         Scene scene = new Scene(mainBox);
         this.setScene(scene);
         this.setTitle("Visualisation données");
