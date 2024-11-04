9f369247286f99cd65d53ac5975891d37b07f855
Dylan Lecocq <dylan.lecocq.etu@univ-lille.fr>
2024-11-04 16:07:39 +0100

    Suppression d'exception non lancée

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,11 +3,9 @@ package app;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         new UserInterface();
     }
 
