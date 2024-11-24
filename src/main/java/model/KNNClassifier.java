ebcffd0cd1176571079965bd1acfb5919b18aca7
Giorgio UTZERI <giorgio.utzeri.etu@univ-lille.fr>
2024-11-24 22:26:07 +0100

    JavaDoc KNN

--- src/main/java/model/KNNClassifier.java
+++ src/main/java/model/KNNClassifier.java
@@ -2,18 +2,42 @@ package model;
 
 import java.util.*;
 
+/**
+ * La classe KNNClassifier implémente l'algorithme des K plus proches voisins
+ * pour classifier des points de données en fonction de leur proximité avec d'autres points.
+ */
 public class KNNClassifier {
     private final static Random RAND = new Random();
 
+    /**
+     * Associe une catégorie aléatoire à un point donné.
+     *
+     * @param pt le point à classifier aléatoirement.
+     */
     public void randomClassify(IrisPoint pt) {
         Category[] values = Category.values();
         pt.setCategory(values[RAND.nextInt(values.length)]);
     }
-
+    /**
+     * Classifie un point donné en utilisant l'algorithme des K plus proches voisins.
+     *
+     * @param point le point à classifier.
+     * @param k le nombre de voisins à considérer.
+     * @param d la distance à utiliser pour mesurer la proximité (euclidienne, manhattan).
+     * @param datas la liste des points de données déjà classifiés.
+     */
     public void classify(IrisPoint point, int k, Distance d, List<IrisPoint> datas) {
         point.setCategory(this.determineCategory(point, k, d, datas));
     }
-
+    /**
+     * Trouve les K plus proches voisins d'un point donné en fonction d'une distance.
+     *
+     * @param k le nombre de voisins à rechercher.
+     * @param p le point pour lequel trouver les voisins.
+     * @param d la distance à utiliser pour mesurer la proximité.
+     * @param datas la liste des points de données à examiner.
+     * @return un tableau contenant les K plus proches voisins.
+     */
     public IrisPoint[] kPlusProchesVoisins(int k, IrisPoint p, Distance d, List<IrisPoint> datas) {
         SortedMap<Double, List<IrisPoint>> distances = new TreeMap<>();
 
@@ -43,7 +67,13 @@ public class KNNClassifier {
 
         return voisins;
     }
-
+    /**
+     * Calcule la meilleure valeur de K pour maximiser le taux de classification correct.
+     *
+     * @param d la distance à utiliser pour mesurer la proximité.
+     * @param data la liste des points de données à utiliser pour le calcul.
+     * @return la valeur optimale de K.
+     */
     public int getBestKValue(Distance d, List<IrisPoint> data) {
         int k = 3;
         double maxSuccessRate = 0;
@@ -62,7 +92,15 @@ public class KNNClassifier {
         }
         return bestKValue;
     }
-
+    /**
+     * Détermine la catégorie d'un point donné en fonction des K plus proches voisins.
+     *
+     * @param p le point à classifier.
+     * @param k le nombre de voisins à considérer.
+     * @param d la distance à utiliser pour mesurer la proximité.
+     * @param datas la liste des points de données à examiner.
+     * @return la catégorie déterminée pour le point.
+     */
     public Category determineCategory(IrisPoint p, int k, Distance d, List<IrisPoint> datas) {
         IrisPoint[] voisins = kPlusProchesVoisins(k, p, d, datas);
         HashMap<Category, Integer> nombreParCategorie = new HashMap<>();
