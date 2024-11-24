be24b9b6b0d3a3a7f30c2d6346d8f7339f83e467
Giorgio UTZERI <giorgio.utzeri.etu@univ-lille.fr>
2024-11-24 22:27:28 +0100

    JavaDoc ManhattanDistance

--- src/main/java/model/ManhattanDistance.java
+++ src/main/java/model/ManhattanDistance.java
@@ -1,6 +1,13 @@
 package model;
 
 public class ManhattanDistance implements Distance {
+    /**
+     * Calcule la distance de Manhattan entre deux points Iris.
+     *
+     * @param p1 le premier point de données.
+     * @param p2 le second point de données.
+     * @return la distance de Manhattan entre les deux points.
+     */
     @Override
     public double distance(IrisPoint p1, IrisPoint p2) {
         return p1.getPetalWidthDifference(p2) + p1.getPetalLengthDifference(p2) +
