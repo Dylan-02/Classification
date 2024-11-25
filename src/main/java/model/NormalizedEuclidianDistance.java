ced9f5c7545b0bceb2c6f32a7131e3df0a67905f
Giorgio UTZERI <giorgio.utzeri.etu@univ-lille.fr>
2024-11-25 10:07:59 +0100

    JavaDoc NormalizedDistanceEuclidian

--- src/main/java/model/NormalizedEuclidianDistance.java
+++ src/main/java/model/NormalizedEuclidianDistance.java
@@ -1,11 +1,22 @@
 package model;
 
+/**
+ * Cette class permet de normalisé les données des IrisPoint pour calculer la distance Euclidiene
+ */
 public class NormalizedEuclidianDistance implements Distance {
     protected double sepalLengthAmplitude;
     protected double sepalWidthAmplitude;
     protected double petalLengthAmplitude;
     protected double petalWidthAmplitude;
 
+    /**
+     * Constructeur permettant de définir les données d'un IrisPoint
+     *
+     * @param sepalLengthAmplitude Longueur du Sepal de l'Iris
+     * @param sepalWidthAmplitude Largeur du Sepal de l'Iris
+     * @param petalLengthAmplitude Longueur de la Pétal de l'Iris
+     * @param petalWidthAmplitude Largeur de la Pétal de l'Iris
+     */
     public NormalizedEuclidianDistance(double sepalLengthAmplitude, double sepalWidthAmplitude, double petalLengthAmplitude, double petalWidthAmplitude) {
         this.sepalLengthAmplitude = sepalLengthAmplitude;
         this.sepalWidthAmplitude = sepalWidthAmplitude;
@@ -13,6 +24,13 @@ public class NormalizedEuclidianDistance implements Distance {
         this.petalWidthAmplitude = petalWidthAmplitude;
     }
 
+    /**
+     * Permet de normalisé la distance entre IrisPoint de toutes les données.
+     *
+     * @param p1 IrisPoint à comparer
+     * @param p2 IrisPoint a normalisé avec le premier
+     * @return la distance normalisé entre les deux point (double)
+     */
     @Override
     public double distance(IrisPoint p1, IrisPoint p2) {
         double result = Math.pow(Math.abs((p1.getSepalLengthDifference(p2))/this.sepalLengthAmplitude), 2) + Math.pow(Math.abs((p1.getSepalWidthDifference(p2))/this.sepalWidthAmplitude), 2)
